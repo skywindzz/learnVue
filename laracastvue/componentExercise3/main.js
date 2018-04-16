05675d536acc26ae759044982a5b0006622e36d3
skywindzz <skywindzz@yahoo.com>
2018-04-15 23:09:39 -0700

    4-15

--- laracastvue/componentExercise3/main.js
+++ laracastvue/componentExercise3/main.js
@@ -1,9 +1,65 @@
 
+Vue.component('tabs', {
+    template: 
+    `<div>
+        <div class="tabs">
+            <ul>
+                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                  <a :href="tab.href" @click="selectTab(tab)"> {{ tab.name }} </a>
+                </li>
+            </ul>
+        </div>
 
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>`,
+
+    data() {
+        return { tabs: [] } ;
+    },
+
+    created() {
+        this.tabs = this.$children
+    },
+
+    methods: {
+        selectTab(selectedTab) { 
+             this.tabs.forEach(tab => {
+                 tab.isActive = (tab.name == selectedTab.name)
+             })
+        }
+    }
+});
+
+
+Vue.component('tab', {
+    template: `<div v-show="isActive"><slot></slot></div>`,
+    props: {
+        name: { required: true },
+        isSelected: { default: false }
+    },
+
+    data() {
+        return {
+            isActive: false
+        }
+    },
+
+    mounted() {
+        this.isActive = this.isSelected
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    }
+});
 
 new Vue ({
     el: "#root",
     data: {
-        isSelected: false
+        ok: true
     }
 })
