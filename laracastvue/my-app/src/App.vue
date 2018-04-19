329c88abf45e95027c19a3edf455028f8ee22ec3
skywindzz <skywindzz@yahoo.com>
2018-04-18 22:04:11 -0700

    4-18

---
+++ laracastvue/my-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <message>Hello there</message>
+    <message>Hey hey</message>
+  </div>
+</template>
+
+<script>
+import message from './components/message.vue';
+
+export default {
+  name: 'app',
+  components: {
+    message
+  }
+}
+</script>
+
---
+++ laracastvue/my-app/src/components/message.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="box-info">
+    <p>
+        <slot></slot>
+    </p>
+  </div>  
+</template>
+
+
+export default {
+  /* data or methods here if needed */
+}
+
+<style>
+    .box-info {
+        background-color: lightgreen;
+        border: 1px solid;
+        padding: 10px;
+        margin-bottom: 1rem;
+    }
+</style>
