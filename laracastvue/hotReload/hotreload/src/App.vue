6ed49d7ec5408fcaa4fb5f1d26220f710f966731
skywindzz <skywindzz@yahoo.com>
2018-04-21 01:42:26 -0700

    4/21

---
+++ laracastvue/hotReload/hotreload/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
---
+++ laracastvue/hotReload/hotreload/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>the count is {{ count }}</p>
+    <button @click="count +=1">add 1</button>
+
+    <div>
+      <p style="white-space: pre-line;"> {{message}} </p>
+      <textarea v-model="message" placeholder="input something"></textarea>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      count: 0,
+      message: ''
+    }
+  }
+}
+
+</script>
