5e25b839a37669577471126016c9ddb1b560eca4
skywindzz <skywindzz@yahoo.com>
2018-05-09 01:09:34 -0700

    may 9

---
+++ vueProject/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+        <p>vue is {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">change</button>
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+            return { status: 'not running'}
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'running'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
