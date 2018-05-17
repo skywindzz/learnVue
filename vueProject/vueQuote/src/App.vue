2b3e20390a8660e7f603cb3771535f7204d12576
skywindzz <skywindzz@yahoo.com>
2018-05-16 17:17:04 -0700

    5/16

--- vueProject/vueQuote/src/App.vue
+++ vueProject/vueQuote/src/App.vue
@@ -2,25 +2,39 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
+               <button @click="selectedComponent='appQuote'">Quote</button>
+               <button @click="selectedComponent='appAuthor'">Content</button>
+               <button @click="selectedComponent='appNew'">Title</button>
+               <hr> 
+               <p>{{ selectedComponent }}</p>
+               <keep-alive>
+                    <component :is="selectedComponent"></component>
+               </keep-alive>
+               <!-- <app-quote>
                    <h3 slot="title">{{ slotContent }}</h3>
                    <p slot="content">testing slot content</p>
-               </app-quote>
+               </app-quote> -->
             </div>
         </div>
-    </div>
+    </div> 
 </template>
 
 <script>
     import Quote from "./components/Quote.vue"
+    import Author from "./components/Author.vue"
+    import New from "./components/New.vue"
+
     export default {
           data() {
               return {
-                  slotContent: 'Slot content'
+                  slotContent: 'Slot content',
+                  selectedComponent: 'appQuote'
               }
           },
           components: {
-              appQuote: Quote
+              appQuote: Quote,
+              appAuthor: Author,
+              appNew: New
           }
     }
 </script>
---
+++ vueProject/vueQuote/src/components/New.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Plus one</button>
+    <p>counter: {{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          counter: 0
+      }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
