ff35abd6a203acb8d4ef2048e0ce890e6815545d
skywindzz <skywindzz@yahoo.com>
2018-06-13 17:09:39 -0700

    6-13

--- vueProject/animationTransition/src/App.vue
+++ vueProject/animationTransition/src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
+                <h1>Animation</h1>
                 <hr>
                 <button class="btn btn-primary" @click="show = !show">show alert</button>
                 <br></br>
